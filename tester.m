5eb974f45444d29c6029f148875c335868df6ee2
Yang, Jia-Shu <yangjiashu2015@outlook.com>
2024-11-24 19:30:43 +0800

    ADD REFERENCES INTO THE MAIN SCRIPT

--- tester.m
+++ tester.m
@@ -53,3 +53,10 @@ histogram(asgn_prob,20);
 plot(rps(:,1),rps(:,2),'o','LineWidth',1,'MarkerSize',5);
 xlabel('\itX_{\rm1}'); xlabel('\itX_{\rm1}');
 set(gca, 'FontName','Arial','FontSize',12);
+
+%% REFERENCES
+%----------------------------------------------------------------------
+Chen JB, Yang JY, Li J. (2016) A GF-discrepancy for point selection in stochastic seismic response analysis of structures with uncertain parameters. Structural Safety 59: 20-31. https://doi.org/10.1016/j.strusafe.2015.11.001
+Chen JB, Yang JS, Jensen HA. (2020) Structural optimization considering dynamic reliability constraints via probability density evolution method and change of probability measure. Struct Multidisc Optim 62: 2499–2516. https://doi.org/10.1007/s00158-020-02621-4
+Yang JS, Jensen HA, Chen JB. (2022) Structural optimization under dynamic reliability constraints utilizing probability density evolution method and metamodels in augmented input space. Struct Multidisc Optim 65: 107. https://doi.org/10.1007/s00158-022-03188-y
+Yang JS, Chen JB, Jensen HA. (2022) Structural design optimization under dynamic reliability constraints based on the probability density evolution method and highly-efficient sensitivity analysis. Probab Eng Mech 68: 103205. https://doi.org/10.1016/j.probengmech.2022.103205
